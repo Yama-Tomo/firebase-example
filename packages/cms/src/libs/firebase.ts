976a23f2b916a4cdceb122eeb7b77f2592dd4849
Yama-Tomo <mazda13bturbo@gmail.com>
2020-04-05 18:34:34 +0900

    feat: 永続化するためのデータにタイムスタンプのプロパティを追加するユーティリティを作成した

--- packages/cms/src/libs/firebase.ts
+++ packages/cms/src/libs/firebase.ts
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { Unpacked } from '~/types';
 
 export const initializeFirebase = () => {
   if (firebase.apps.length) {
@@ -16,3 +18,37 @@ export const initializeFirebase = () => {
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
   });
 };
+
+const FieldValue = firebase.firestore.FieldValue;
+
+const defaultAppendProperties = ['updated_at', 'created_at'] as const;
+
+// const result = appendTimestamp({aa: 1, bb: 'aaa'}) => { aa: number, bb: string, updated_at: Timestamp, created_at: Timestamp }
+export function appendTimestamp<T>(
+  data: T
+): T & Record<Unpacked<typeof defaultAppendProperties>, firebase.firestore.Timestamp>;
+
+// const result = appendTimestamp({aa: 1, bb: 'aaa'}, (['hoge'] as const)) => { aa: number, bb: string, hoge: Timestamp }
+export function appendTimestamp<T, F extends readonly string[]>(
+  data: T,
+  fields: F
+): T & Record<Unpacked<F>, firebase.firestore.Timestamp>;
+
+export function appendTimestamp<T, F extends readonly string[]>(
+  data: T,
+  fields?: F
+): T & Record<Unpacked<F>, firebase.firestore.Timestamp> {
+  const appendData = (fields || (defaultAppendProperties as readonly string[])).reduce(
+    (result, current) => {
+      const key = current as keyof typeof result;
+
+      return {
+        ...result,
+        [key]: FieldValue.serverTimestamp() as firebase.firestore.Timestamp,
+      };
+    },
+    {} as Record<Unpacked<F>, firebase.firestore.Timestamp>
+  );
+
+  return { ...data, ...appendData };
+}
