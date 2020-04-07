e595ffc787a98660be9c04e10fb58d3ecb1a9cfd
Yama-Tomo <mazda13bturbo@gmail.com>
2020-04-07 17:36:25 +0900

    feat: graphql の型定義ファイルを自動生成するプラグインを追加

--- packages/site/gatsby-config.js
+++ packages/site/gatsby-config.js
@@ -4,5 +4,13 @@ const activeEnv = process.env.ACTIVE_ENV || process.env.NODE_ENV || 'development
 dotenv.config({ path: `.env.${activeEnv}` })
 
 module.exports = {
-  plugins: ['gatsby-plugin-typescript']
+  plugins: [
+    'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-graphql-codegen',
+      options: {
+        fileName: 'graphql.d.ts',
+      }
+    }
+  ]
 }
