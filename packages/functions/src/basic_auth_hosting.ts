1310dd0586181939ba2da4818dffed0373a6112a
Yama-Tomo <mazda13bturbo@gmail.com>
2020-04-08 14:30:28 +0900

    feat: basic認証をかけてhostingできるようにするfunctionを作成した

---
+++ packages/functions/src/basic_auth_hosting.ts
@@ -0,0 +1,20 @@
+import * as path from 'path';
+import * as functions from 'firebase-functions';
+import * as express from 'express';
+
+const basicAuth = require('basic-auth-connect');
+
+const user = functions.config().basicauth.user;
+const pass = functions.config().basicauth.pass;
+
+const serverCreator = (user: string, pass: string, hostingPath: string) => {
+  const server = express();
+  server.use(basicAuth(user, pass));
+  server.use(express.static(path.resolve(hostingPath)));
+
+  return server;
+};
+
+export const basicAuthSiteHosting = functions.https.onRequest(
+  serverCreator(user, pass, './static/site')
+);
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,2 +1,3 @@
 export * from './sync_public_articles';
 export * from './assign_admin_role';
+export * from './basic_auth_hosting';
