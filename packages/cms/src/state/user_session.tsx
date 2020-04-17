668015c4d30a7de95f93c0ef9b67b316e235da2e
Yama-Tomo <mazda13bturbo@gmail.com>
2020-04-17 18:08:18 +0900

    feat: Admin SDK で払い出したカスタムトークンをクエリストリングにつけてアクセスしたら認証するようにした

--- packages/cms/src/state/user_session.tsx
+++ packages/cms/src/state/user_session.tsx
@@ -1,5 +1,7 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useRef, useState } from 'react';
 import firebase from 'firebase/app';
+import { useHistory } from 'react-router-dom';
+import qs from 'qs';
 
 type State = (firebase.User & { isAdmin: boolean }) | false | undefined;
 
@@ -9,20 +11,42 @@ const userSessionStore = createContext<State>(initialState);
 const { Provider } = userSessionStore;
 
 const SessionStateProvider = ({ children }: { children: React.ReactChild }) => {
+  const verified = useRef(false);
+  const history = useHistory();
   const [state, setState] = useState<State>(initialState);
+  const params: Record<'token', string | undefined> = qs.parse(history.location.search, {
+    ignoreQueryPrefix: true,
+  });
+
+  const checkCredential = (user: firebase.User | null) => {
+    if (!user) {
+      setState(false);
+      return;
+    }
+
+    user.getIdTokenResult(true).then((v) => {
+      setState({ ...user, isAdmin: !!v.claims.admin });
+    });
+  };
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      if (!user) {
-        setState(false);
-        return;
-      }
+    if (verified.current) {
+      return;
+    }
+    verified.current = true;
 
-      user.getIdTokenResult(true).then((v) => {
-        setState({ ...user, isAdmin: !!v.claims.admin });
-      });
-    });
-  }, []);
+    if (params.token) {
+      firebase
+        .auth()
+        .signInWithCustomToken(params.token)
+        .then(({ user }) => {
+          checkCredential(user);
+        });
+      return;
+    }
+
+    firebase.auth().onAuthStateChanged(checkCredential);
+  }, [params.token]);
 
   return <Provider value={state}>{children}</Provider>;
 };
