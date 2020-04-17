3987a8e69d018eca3d9661e9d76664f413f633a5
Yama-Tomo <mazda13bturbo@gmail.com>
2020-04-17 11:02:11 +0900

    chore: types をパッケージ化

--- packages/cms/src/types/react/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from 'react';
-
-declare module 'react' {
-  type FCX<P = {}> = FunctionComponent<P & { className?: string }>;
-}
-
-export type ContextStateType<T> = T extends React.Context<infer R> ? R : never;
--- packages/types/react/index.d.ts
+++ packages/types/react/index.d.ts
@@ -3,3 +3,5 @@ import { FunctionComponent } from 'react';
 declare module 'react' {
   type FCX<P = {}> = FunctionComponent<P & { className?: string }>;
 }
+
+export type ContextStateType<T> = T extends React.Context<infer R> ? R : never;
